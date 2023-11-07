7a3ae26d9bd1061b953545920587e8b8e429c2be
LidorLazar <lidorlazar33@gmail.com>
2023-11-07 22:18:40 +0200

    add DB and connection

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,12 @@
 import NextAuth from "next-auth"
 import Auth0Provider from 'next-auth/providers/auth0';
+import {db} from "@/lib/db";
+import {PrismaAdapter} from "@next-auth/prisma-adapter";
+
 
 
 const handler = NextAuth({
+    adapter: PrismaAdapter(db),
     providers: [
         Auth0Provider({
             clientId: process.env.AUTH0_CLIENT_ID!,
@@ -11,12 +15,12 @@ const handler = NextAuth({
 
 
         })],
-    // callbacks: {
-    //     async session({session, user}) {
-    //         session.user.role = user.role
-    //         return session
-    //     }
-    // }
+    callbacks: {
+        async session({session, user}) {
+            session.user.role = user.role
+            return session
+        }
+    }
 
 })
 
